70e932f1e2719e16e0733ea34c31eb13df1eb17c
Emilio-Camino <adrian.camino@epn.edu.ec>
2025-11-08 08:18:09 -0500

    Testing Apache Compatibility with Git

---
+++ SistemaFacturacion/src/main/java/com/sistemafacturacion/SistemaFacturacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sistemafacturacion;
+
+/**
+ *
+ * @author emili
+ */
+public class SistemaFacturacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
