3fe21474f95c3900a356c1f42d2ad8d8a92927a7
Emilio-Camino <adrian.camino@epn.edu.ec>
2025-11-09 11:30:36 -0500

    QuickFix: mising method in the interface

--- SistemaFacturacion/src/main/java/facturacion/gestores/interfaces/IGestorClienteHeladero.java
+++ SistemaFacturacion/src/main/java/facturacion/gestores/interfaces/IGestorClienteHeladero.java
@@ -3,6 +3,7 @@ package facturacion.gestores.interfaces;
 import facturacion.elementos.Cliente;
 
 public interface IGestorClienteHeladero {
+    public Cliente buscarCliente(String cedula);
     public void modificarCliente(String cedula);
     public void eliminarCliente(String cedula);
 }
